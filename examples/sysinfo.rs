9cd945411ff91a4448b0206f6371176be34b76c8
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-08-09 15:30:24 -0500

    Simplify sysinfo example

--- examples/sysinfo.rs
+++ examples/sysinfo.rs
@@ -3,30 +3,21 @@ use std::time::Duration;
 use sysinfo::{CpuExt, System, SystemExt};
 use tokio::sync::{mpsc, oneshot};
 
-enum SystemRequest {
-    Cpu(oneshot::Sender<f32>),
-    // Other possible system queries here
-}
-
-fn system_task() -> mpsc::Sender<SystemRequest> {
+fn cpu_task() -> mpsc::Sender<oneshot::Sender<f32>> {
     let mut sys = System::new();
     let mut cpu_opt = EphemeralOption::new_empty(Duration::from_secs(1));
-    let (tx, mut rx) = mpsc::channel(10);
+    let (tx, mut rx) = mpsc::channel::<oneshot::Sender<f32>>(10);
     tokio::task::spawn(async move {
-        while let Some(request) = rx.recv().await {
-            match request {
-                SystemRequest::Cpu(channel) => {
-                    if let Some(&cpu) = cpu_opt.get() {
-                        eprintln!("using cached CPU data");
-                        channel.send(cpu).unwrap();
-                    } else {
-                        sys.refresh_cpu();
-                        let cpu = sys.global_cpu_info().cpu_usage();
-                        channel.send(cpu).unwrap();
-                        cpu_opt.insert(cpu);
-                    }
-                }
-            };
+        while let Some(channel) = rx.recv().await {
+            if let Some(&cpu) = cpu_opt.get() {
+                eprintln!("using cached CPU data");
+                channel.send(cpu).unwrap();
+            } else {
+                sys.refresh_cpu();
+                let cpu = sys.global_cpu_info().cpu_usage();
+                channel.send(cpu).unwrap();
+                cpu_opt.insert(cpu);
+            }
         }
     });
     tx
@@ -34,14 +25,14 @@ fn system_task() -> mpsc::Sender<SystemRequest> {
 
 #[tokio::main]
 async fn main() {
-    let sys_tx = system_task();
+    let sys_tx = cpu_task();
 
     for _ in 0..3 {
         let sys_tx = sys_tx.clone();
         tokio::spawn(async move {
             loop {
                 let (resp_tx, resp_rx) = oneshot::channel();
-                sys_tx.send(SystemRequest::Cpu(resp_tx)).await.unwrap();
+                sys_tx.send(resp_tx).await.unwrap();
                 println!("CPU usage: {}", resp_rx.await.unwrap());
                 tokio::time::sleep(Duration::from_millis(250)).await;
             }
