72f1fe866b32269435cdc4603b5114c1ba87e843
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-08-09 15:20:58 -0500

    Update simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut num_opt = EphemeralOption::new(0, std::time::Duration::from_secs(1));
     loop {
         match num_opt.get() {
-            Some(num) => println!("{num}"),
+            Some(&num) => println!("{num}"),
             None => {
                 let prev_num = num_opt.get_expired().unwrap();
                 let num = num_opt.insert(prev_num + 1);
