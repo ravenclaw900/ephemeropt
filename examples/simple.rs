de03f28334d0b0ad1590e27527689ac01ad97de8
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-08-09 15:18:02 -0500

    Add simpler example

---
+++ examples/simple.rs
@@ -0,0 +1,16 @@
+use ephemeropt::EphemeralOption;
+
+fn main() {
+    let mut num_opt = EphemeralOption::new(0, std::time::Duration::from_secs(1));
+    loop {
+        match num_opt.get() {
+            Some(num) => println!("{num}"),
+            None => {
+                let prev_num = num_opt.get_expired().unwrap();
+                let num = num_opt.insert(prev_num + 1);
+                println!("{num}");
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+}
